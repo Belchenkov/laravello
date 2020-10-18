257acc4ab7f36ad0e8b6f9ff053364291414f184
Aleksey Belchenkov <u608110@gmail.com>
2020-10-18 22:00:47 +0300

    Create Models & Relations

---
+++ app/Models/Board.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Board extends Model
+{
+    use HasFactory;
+
+    public function lists(): HasMany
+    {
+        return $this->hasMany(CardList::class, 'board_id');
+    }
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
---
+++ app/Models/Card.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Card extends Model
+{
+    use HasFactory;
+
+    public function list(): BelongsTo
+    {
+        return $this->belongsTo(CardList::class, 'list_id');
+    }
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
