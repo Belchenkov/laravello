4065b7737d7113d32f4c14b1b359fe4c56bce90d
Aleksey Belchenkov <u608110@gmail.com>
2020-11-06 14:09:39 +0300

    Add Vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 
 import './bootstrap';
+import store from './vuex.config';
 import router from "./router";
 import apolloProvider from './apollo.config';
 import Board from "./pages/Board";
@@ -11,6 +12,7 @@ Vue.component('board', Board);
 
 const app = new Vue({
     el: '#app',
+    store,
     router,
     apolloProvider
 });
