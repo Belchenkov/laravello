b3e1f77264d9b474baf61fb454642ceca4bffcbc
Aleksey Belchenkov <u608110@gmail.com>
2020-11-18 15:24:29 +0300

    Authorizing Board

--- public/js/app.js
+++ public/js/app.js
@@ -6111,6 +6111,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_DropdownMenu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/DropdownMenu */ "./resources/js/components/DropdownMenu.vue");
 /* harmony import */ var _graphql_UserBoards_gql__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./../graphql/UserBoards.gql */ "./resources/js/graphql/UserBoards.gql");
 /* harmony import */ var _graphql_UserBoards_gql__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_graphql_UserBoards_gql__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../utils */ "./resources/js/utils.js");
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 //
 //
 //
@@ -6128,6 +6135,12 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+
 
 
 
@@ -6141,9 +6154,16 @@ __webpack_require__.r(__webpack_exports__);
       showBoards: false
     };
   },
-  computed: Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapState"])({
+  computed: _objectSpread(_objectSpread({}, Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapState"])({
     userId: function userId(state) {
       return state.user.id;
+    }
+  })), {}, {
+    colorMap100: function colorMap100() {
+      return _utils__WEBPACK_IMPORTED_MODULE_3__["colorMap100"];
+    },
+    colorMap200: function colorMap200() {
+      return _utils__WEBPACK_IMPORTED_MODULE_3__["colorMap200"];
     }
   }),
   apollo: {
@@ -33764,22 +33784,23 @@ var render = function() {
             [_vm._v("BOARDS")]
           ),
           _vm._v(" "),
-          _vm._l(8, function(n) {
+          _vm._l(_vm.userBoards, function(board) {
             return _c(
-              "div",
+              "router-link",
               {
-                key: n,
+                key: board.id,
                 staticClass:
-                  "m-2 bg-teal-100 rounded-sm opacity-100 hover:opacity-75 text-gray-700 font-bold cursor-pointer flex"
+                  "m-2 rounded-sm opacity-100 hover:opacity-75 text-gray-700 font-bold cursor-pointer flex",
+                class: ["bg-" + board.color + "-100"],
+                attrs: { to: { name: "board", params: { id: board.id } } }
               },
               [
                 _c("div", {
-                  staticClass: "bg-teal-200 w-10 rounded-sm rounded-r-none"
+                  staticClass: "w-10 rounded-sm rounded-r-none",
+                  class: ["bg-" + board.color + "-200"]
                 }),
                 _vm._v(" "),
-                _c("div", { staticClass: "p-2" }, [
-                  _vm._v("The board " + _vm._s(n + 1))
-                ])
+                _c("div", { staticClass: "p-2" }, [_vm._v(_vm._s(board.title))])
               ]
             )
           })
@@ -53735,7 +53756,7 @@ var routes = [{
 /*!*******************************!*\
   !*** ./resources/js/utils.js ***!
   \*******************************/
-/*! exports provided: gqlErrors, AuthError, colorMap500 */
+/*! exports provided: gqlErrors, AuthError, colorMap500, colorMap100, colorMap200 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -53743,6 +53764,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "gqlErrors", function() { return gqlErrors; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AuthError", function() { return AuthError; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "colorMap500", function() { return colorMap500; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "colorMap100", function() { return colorMap100; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "colorMap200", function() { return colorMap200; });
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -53832,6 +53855,28 @@ var colorMap500 = {
   green: "bg-green-500",
   blue: "bg-blue-500",
   indigo: "bg-indigo-500"
+};
+var colorMap100 = {
+  teal: "bg-teal-100",
+  orange: "bg-orange-100",
+  gray: "bg-gray-100",
+  yellow: "bg-yellow-100",
+  purple: "bg-purple-100",
+  red: "bg-red-100",
+  green: "bg-green-100",
+  blue: "bg-blue-100",
+  indigo: "bg-indigo-100"
+};
+var colorMap200 = {
+  teal: "bg-teal-200",
+  orange: "bg-orange-200",
+  gray: "bg-gray-200",
+  yellow: "bg-yellow-200",
+  purple: "bg-purple-200",
+  red: "bg-red-200",
+  green: "bg-green-200",
+  blue: "bg-blue-200",
+  indigo: "bg-indigo-200"
 };
 
 /***/ }),
--- resources/js/components/UserBoardsDropdown.vue
+++ resources/js/components/UserBoardsDropdown.vue
@@ -3,22 +3,29 @@
         <button class="header-btn" @click="showBoards = !showBoards">Boards</button>
         <dropdown-menu :show="showBoards">
             <div class="text-gray-600 text-xs font-semibold mb-2 ml-2">BOARDS</div>
-            <div
-                v-for="n in 8"
-                :key="n"
-                class="m-2 bg-teal-100 rounded-sm opacity-100 hover:opacity-75 text-gray-700 font-bold cursor-pointer flex"
+            <router-link
+                :to="{ name: 'board', params: { id: board.id } }"
+                v-for="board in userBoards"
+                :key="board.id"
+                :class="[`bg-${board.color}-100`]"
+                class="m-2 rounded-sm opacity-100 hover:opacity-75 text-gray-700 font-bold cursor-pointer flex"
             >
-                <div class="bg-teal-200 w-10 rounded-sm rounded-r-none"></div>
-                <div class="p-2">The board {{ n + 1 }}</div>
-            </div>
+                <div
+                    :class="[`bg-${board.color}-200`]"
+                    class="w-10 rounded-sm rounded-r-none"
+                ></div>
+                <div class="p-2">{{ board.title }}</div>
+            </router-link>
         </dropdown-menu>
     </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+
 import DropdownMenu from "../components/DropdownMenu";
 import UserBoards from "./../graphql/UserBoards.gql"
+import { colorMap100, colorMap200 } from "../utils";
 
 export default {
     name: "UserBoardsDropdown",
@@ -30,9 +37,13 @@ export default {
             showBoards: false
         }
     },
-    computed: mapState({
-        userId: state => state.user.id
-    }),
+    computed: {
+        ...mapState({
+            userId: state => state.user.id
+        }),
+        colorMap100: () => colorMap100,
+        colorMap200: () => colorMap200
+    },
     apollo: {
         userBoards: {
             query: UserBoards,
